f4a988bc32cf8f531153c90c2a273e1887bb38b7
Krishna Rathod <krishnarathod7089@gmail.com>
2025-08-15 00:20:15 +0530

    express crud

--- app.js
+++ app.js
@@ -1,10 +1,60 @@
-const logger = require("./logger.js")
-const utils = require("./utils.js")
+const express = require('express');
+const app = express();
 
-logger.log("Hello World");
-const sum = utils.add(1, 2);
-const substraction = utils.subtract(1, 2);
-logger.log(sum);
-logger.log(substraction);
+app.use(express.json());
 
-logger.error("This is an error message");
+const tasks = [
+    { id: 1, title: "Task 1", description: "Description 1" },
+    { id: 2, title: "Task 2", description: "Description 2" },
+    { id: 3, title: "Task 3", description: "Description 3" },
+];
+
+
+const newId = tasks.length + 1;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+});
+
+app.post("/task", (req, res) => {
+    console.log(req.body)
+    const newTask = {
+        id: newId,
+        title: req.body.title,
+        description: req.body.description,
+    };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+});
+
+app.put("/task/:id", (req, res) => {
+    const taskId = parseInt(req.params.id)
+    const { title, description } = req.body
+    const task = tasks.find(t => t.id === taskId)
+    if (!task) {
+        return res.status((404).json({ error: "task not found" }))
+    }
+    if (title !== undefined) task.title = title
+    if (description !== undefined) task.description = description
+
+    res.json(task)
+})
+
+app.delete("/task/:id", (req, res) => {
+    const taskId = parseInt(req.params.id)
+    const index = tasks.findIndex(t => t.id === taskId)
+    if (index === -1) {
+        return res.status((404).json({ error: "task not found" }))
+    }
+    const deleteTask = tasks.splice(index, 1)[0]
+    res.json(deleteTask)
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
